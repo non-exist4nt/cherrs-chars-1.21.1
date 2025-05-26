5e3135222c70df98689dee0dcec7aa52407ca46a
cherr <cohen.m.janczak@gmail.com>
2025-05-26 04:57:52 -0500

    added and removed some silly comments

--- src/main/java/net/nonnie/cherrschars/CherrsChars.java
+++ src/main/java/net/nonnie/cherrschars/CherrsChars.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//hi there
 public class CherrsChars implements ModInitializer {
 	public static final String MOD_ID="cherrschars";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
--- src/main/java/net/nonnie/cherrschars/mixin/ExampleMixin.java
+++ src/main/java/net/nonnie/cherrschars/mixin/ExampleMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
